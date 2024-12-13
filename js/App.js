756f15c6162e8543c8aa9a0198e30cc24a777742
NurlanAmanov <nurlanemenov14@gmail.com>
2024-12-13 12:59:31 +0400

    kuryer-loginv-1

--- js/App.js
+++ js/App.js
@@ -1,7 +1,8 @@
 var toggleOpen = document.getElementById('toggleOpen');
 var toggleClose = document.getElementById('toggleClose');
 var collapseMenu = document.getElementById('collapseMenu');
-
+const passvisb = document.getElementById("passvisb")
+const imgvisb = document.getElementById("imgvisb")
 function handleClick() {
   if (collapseMenu.style.display === 'block') {
     collapseMenu.style.display = 'none';
@@ -13,9 +14,18 @@ function handleClick() {
 toggleOpen.addEventListener('click', handleClick);
 toggleClose.addEventListener('click', handleClick);
 
-const loginpanel =document.getElementById("loginpanel")
-const regstrpage =document.getElementById("regstrpage")
+const loginpanel = document.getElementById("loginpanel")
+const regstrpage = document.getElementById("regstrpage")
 
-function openhesab(){loginpanel.style.display="flex"}
-function closehesab(){loginpanel.style.display="none";regstrpage.style.display='none'}
-function regstpanel(){regstrpage.style.display="flex"}
+function openhesab() { loginpanel.style.display = "flex" }
+function closehesab() { loginpanel.style.display = "none"; regstrpage.style.display = 'none' }
+function regstpanel() { regstrpage.style.display = "flex" }
+function changepasvisb() {
+  passvisb.type === "password" ? passvisb.type = "text" : passvisb.type = "password"
+  if (imgvisb.src.includes("view.png")) {
+    imgvisb.src = "/img/hide.png";  
+  } else {
+    imgvisb.src = "/img/view.png"; 
+  }
+  
+}
