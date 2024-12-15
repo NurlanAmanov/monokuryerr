77b35e693b7cbb2975423e8accfd8e901330c6bc
NurlanAmanov <nurlanemenov14@gmail.com>
2024-12-15 12:06:50 +0400

    regstrv-3

---
+++ js/Registr.js
@@ -0,0 +1,28 @@
+function updateText(role) {
+    const roledescription = document.getElementById('roledescription');
+    const parttext = document.getElementById('parttext'); 
+    const kuryer = document.getElementById('kuryer'); 
+    const partnyor = document.getElementById('partnyor'); 
+
+    if (role === 'kuryer') {
+
+        kuryer.style.display = "block";
+        partnyor.style.display = "none";
+        roledescription.innerText = 'Kuryer olaraq qeydiyyatdan keçərək sifarişlərin çatdırılması üzrə fəaliyyət göstərə bilərsiniz.';
+    } else if (role === 'partnyor') {
+       
+        partnyor.style.display = "block";
+        kuryer.style.display = "none";
+        parttext.innerText = 'Partnyor olaraq qeydiyyatdan keçərək öz biznesinizi inkişaf etdirə bilərsiniz.';
+    } else {
+     
+        roledescription.innerText = '';
+        kuryer.style.display = "block";
+        partnyor.style.display = "none";
+    }
+}
+
+window.onload = function () {
+    document.getElementById('kuryer-radio').checked = true; 
+    updateText('kuryer'); 
+};
